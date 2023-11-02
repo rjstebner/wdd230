1cdd110397c4663c565d2f85ede732ce381d8e5b
rjstebner <rjstebner@gmail.com>
2023-11-02 14:29:33 +0900

    join page finished

--- chamber/sctipts/getdates.js
+++ chamber/sctipts/getdates.js
@@ -8,3 +8,8 @@ const lastModified = new Date(document.lastModified);
 const formattedLastModified = 'Last Modified:' + lastModified.toLocaleDateString('en-UK');
 
 document.getElementById('lastmodified').textContent = formattedLastModified;
+
+const timestampField = document.getElementById('timestamp');
+if (timestampField) {
+  timestampField.value = lastModified.toISOString(); // You can format this as needed
+}
